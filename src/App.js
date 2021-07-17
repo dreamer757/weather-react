9788f9113480f5caf3bfddc58dbd54cda9cf26d2
dreamer757 <30564161+dreamer757@users.noreply.github.com>
2021-07-17 15:15:42 -0400

    added axios integration/weather display

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Weather from "./Weather";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello from React</h1>
+          
+          <Weather city="Tokyo"/>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React now!
         </a>
       </header>
     </div>
